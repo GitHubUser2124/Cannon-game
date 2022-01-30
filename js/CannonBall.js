dbe085f1a14adc51eaa8831f0d2932059eea46a2
GitHubUser2124 <97364603+GitHubUser2124@users.noreply.github.com>
2022-01-30 16:56:00 -0500

    Trajectory

--- js/CannonBall.js
+++ js/CannonBall.js
@@ -6,6 +6,7 @@ class CannonBall {
         this.r = 25;
         this.body = Bodies.circle(x, y, this.r, options);
         this.cannonBall_image = loadImage("assets/cannonball.png");
+        this.trajectory = [];
         World.add(world, this.body);
       
     }
@@ -22,14 +23,20 @@ class CannonBall {
 
 
     display(){
-        var pos = this.body.position
-
-
+        var pos = this.body.position;
         push();
         imageMode(CENTER);
         image(this.cannonBall_image, pos.x, pos.y, this.r, this.r);
         pop();
-  
-      
+
+
+        if(this.body.velocity.x>0 && pos.x>20){
+            var position = [pos.x, pos.y];
+            this.trajectory.push(position);
+        }
+
+        for(var i = 0; i<this.trajectory.length; i++){
+            image(this.cannonBall_image, this.trajectory[i][0], this.trajectory[i][1], 5, 5);
+        }
     }
   }
